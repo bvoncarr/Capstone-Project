380ec7a69389c82555da04a9d2ef6f3df6a6f93c
coreycarmichaelcoreycarmichaelcleargit config -- global user.email corey.carmichael@gmail.com <77471705+coreycarmichael@users.noreply.github.com>
2021-07-08 19:21:40 -0400

    added navbar basics

--- tothemoon-app/src/App.js
+++ tothemoon-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Products from './components/Products/Products';
 import Product from './components/Products/Product/Product';
+import Navbar from './components/NavBar/Navbar';
 
 const App = () => {
     return (
         <div>
+            <Navbar />
             <Products />
         </div>
     )
