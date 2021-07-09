874aea0cfe15799e288f88f294a452dac00e1873
bvoncarr <b.voncarr@gmail.com>
2021-07-09 00:54:33 -0400

    renamed folder to client, connected backend to frontend, fixed backend package.json issue

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Products from './components/Products/Products';
+import Product from './components/Products/Product/Product';
+import Navbar from './components/NavBar/Navbar';
+
+
+
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render() {
+      return (
+        <div>
+            <Navbar />
+            <Products />
+                <p className="App-intro">{this.state.apiResponse}</p>
+         </div>
+      );
+  }
+}
+
+export default App;
+
+
+
+
