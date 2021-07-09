ba3a028ee87e626fe006714e1a902e9ad06eaa6d
bvoncarr <b.voncarr@gmail.com>
2021-07-09 12:50:02 -0400

    fixed issue with front-end, backend connected

---
+++ server/index.js
@@ -0,0 +1,25 @@
+//require ('dotenv').config();
+
+var cors = require('cors');
+const http = require('http');
+
+const express = require('express');
+
+const es6Renderer = require('express-es6-template-engine');
+
+const app = express();
+const pgp = require("pg-promise")();
+
+const server = http.createServer(app);
+const path = require('path');
+
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
