0620f726e60e06daf4ccaf0aca5b369fa0302967
Beverly <beverlybuscher@gmail.com>
2021-07-10 14:31:44 -0500

    added scroll functionality to moon and mars product pages

--- client/src/components/Landing-Page/Mars.js
+++ client/src/components/Landing-Page/Mars.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ImageCard from './ImageCard';
 import places from '../../static/places';
+import { Link as Scroll } from 'react-scroll';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,8 +20,10 @@ export default function Mars () {
     const classes = useStyles();
 
     return (
+        <Scroll to="marsProducts" smooth={true}>
         <div className={classes.root} id="mars">
             <ImageCard place={places[1] }/>
         </div>
+        </Scroll>
     )
 }
