5de040f0bff10117f603d6aaef310250be4620a1
coreycarmichaelcoreycarmichaelcleargit config -- global user.email corey.carmichael@gmail.com <77471705+coreycarmichael@users.noreply.github.com>
2021-07-10 13:11:12 -0400

    added routes to the navbar

--- client/src/components/NavBar/Navbar.jsx
+++ client/src/components/NavBar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link, withRouter } from "react-router-dom";
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { Link, withRouter, Router, Switch, BrowserRouter } from "react-router-dom";
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Tab, Tabs } from '@material-ui/core';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 import moon from '../../assets/moon.png';
@@ -9,6 +9,7 @@ import useStyles from './styles';
 
 const Navbar = () => {
     const classes = useStyles();
+    const routes = ["/moonproducts","/marsproducts"]
 
     return (
 
@@ -19,7 +20,20 @@ const Navbar = () => {
                         <img src={moon} alt="To The Moon" height="25px" className={classes.image} />
                         ToThe<span className={classes.titleColor}> Moon</span> 
                     </Typography>
-
+                        <Tabs>
+                            <Tab 
+                                label="Moon"
+                                value={routes[0]}
+                                component={Link}
+                                to={routes[0]}
+                            />
+                            <Tab 
+                                label="Mars"
+                                value={routes[1]}
+                                component={Link}
+                                to={routes[1]}
+                            />
+                        </Tabs>
                     <div className={classes.grow} />
                     <div className={classes.button}>
                         <IconButton aria-label="Show cart items" color="inherit">
