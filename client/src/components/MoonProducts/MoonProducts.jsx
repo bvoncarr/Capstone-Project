8a805a9bf13197036b68f61c6f8e8f14933dadf8
bvoncarr <b.voncarr@gmail.com>
2021-07-13 20:21:33 -0400

    array display on card, updated database, imported useState

--- client/src/components/MoonProducts/MoonProducts.jsx
+++ client/src/components/MoonProducts/MoonProducts.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+
+import React, {useState, useEffect} from 'react';
 import Grid from '@material-ui/core/Grid';
 import MoonProduct from './MoonProduct/MoonProduct';
 import Box from '@material-ui/core/Box';
 
-const fetch = require('node-fetch');
-
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Moon from '../Landing-Page/Moon';
+
+const fetch = require('node-fetch');
+
+
 
 
 const useStyles = makeStyles({
@@ -24,12 +27,15 @@ const useStyles = makeStyles({
 });
 
 
-var getThisData = function () {
-  fetch("http://localhost:3001/moon")
-        .then((res) => res.json())
-        .then((data) => console.log(data));
-}
-console.log(getThisData());
+
+// var getThisData = function () {
+//   fetch("http://localhost:3001/moon")
+//         .then((res) => res.json())
+//         .then((data) => console.log(data));
+// }
+// console.log(getThisData());
+
+
 const products = [
     { id: 1, name: 'Moon Ticket - Single', description: "I'm going to the moon!", price: '$50', image: './images/tickets.jpg'},
     { id: 2, name: 'Family Moon Ticket Package', description: "We're all going to the moon!", price: '$500', image: './images/tickets.jpg'},
@@ -38,6 +44,16 @@ const products = [
 
 const MoonProducts = () => {
   const classes = useStyles();
+  const [moonData, setMoonData] = useState([])
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetch("http://localhost:3001/moon")
+          .then((res) => res.json())
+          .then((data) => setMoonData(data));
+
+
+  },[]);
+  console.log(moonData)
   return (
 
     <>
@@ -48,7 +64,7 @@ const MoonProducts = () => {
     <div id="moonProducts">
      <Box mx={2}  boxShadow={3}>
        <Grid container justifyContent='center' spacing={2}>
-       {products.map((product) => (
+       {moonData.map((product) => (
           <Grid item key={product.id}  xs={12} sm={6} md={4} lg={3}>
             <MoonProduct product={product} />
           </Grid>
