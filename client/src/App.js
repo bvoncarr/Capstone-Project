4891006ab33bfd74b985985f3c263b6a50d64359
coreycarmichaelcoreycarmichaelcleargit config -- global user.email corey.carmichael@gmail.com <77471705+coreycarmichael@users.noreply.github.com>
2021-07-13 21:08:52 -0400

    added a cart to react router

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import MoonProducts from './components/MoonProducts/MoonProducts';
 import MarsProducts from './components/MarsProducts/MarsProducts';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/NavBar/Navbar';
-
+import Cart from './components/Cart/Cart';
+import StripeCheckout from 'react-stripe-checkout';
 import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Header from './components/Landing-Page/Header';
@@ -34,6 +35,7 @@ const App = () => {
               <Route exact path="/" exact component={Header} />
               <Route path="/moonproducts"><MoonProducts /></Route>
               <Route path="/marsproducts"><MarsProducts /></Route>
+              <Route path="/cart"><Cart /></Route>
             </Switch>
          
 
