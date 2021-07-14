ec99384f0b57dd838606719e4b62d98ee86c0bb5
Beverly <beverlybuscher@gmail.com>
2021-07-13 19:59:46 -0500

    updated colors on landing page and descriptions on moon and mars.

--- client/src/static/places.js
+++ client/src/static/places.js
@@ -1,13 +1,13 @@
 const places = [
     {
         title: 'The Moon',
-        description: "Everyone has looked up to the moon and wondered what was up there. How would you like to find out? The moon can be yours!",
+        description: "The Moon is 238,900 miles from Earth. Everyone has looked up to the moon and wondered what was up there. How would you like to find out? The moon can be yours!",
         imageUrl: process.env.PUBLIC_URL + '/images/viewOfEarth.jpeg',
         time: 1500,
     },
     {
         title: 'Mars',
-        description: "Mars is a captivating place that you have to see to believe. Are you ready for the ride of a lifetime? Mars is waiting for you!",
+        description: "Mars is a mind-blowing 232.31 MILLION miles from Earth. It is a captivating planet that you have to see to believe. Are you ready for the ride of a lifetime? Mars is waiting for you!",
         imageUrl: process.env.PUBLIC_URL + '/images/mars2.jpeg',
         time: 1500,
     },
