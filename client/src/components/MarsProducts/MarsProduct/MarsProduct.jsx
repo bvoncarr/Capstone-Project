bef0c593270d6bde3d9ace7e2d7bcff5680cca05
bvoncarr <b.voncarr@gmail.com>
2021-07-20 21:13:17 -0400

    fixed issue

--- client/src/components/MarsProducts/MarsProduct/MarsProduct.jsx
+++ client/src/components/MarsProducts/MarsProduct/MarsProduct.jsx
@@ -9,7 +9,8 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 const MarsProduct = ({ product }) => {
     const classes = useStyles();
-    const [setCart] = useContext(CartContext);
+    // eslint-disable-next-line
+    const [cart, setCart] = useContext(CartContext);
     const addToCart = () => {
 
         const cartProduct = {name: product.company, price: product.price, spaceship: product.spaceship, image: product.pics};
